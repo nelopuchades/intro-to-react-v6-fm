7091f9deb272b8257393999bec7160f81d809983
Nelo Puchades Bresó <correo@nelo.dev>
2021-05-20 23:17:46 +0200

    Added two dropdowns handled via useState hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +19,38 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onBlur={(e) => setAnimal(e.target.value)}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((anim) => (
+              <option value={anim} key={anim}>
+                {anim}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onBlur={(e) => setBreed(e.target.value)}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((bree) => (
+              <option value={bree} key={bree}>
+                {bree}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
