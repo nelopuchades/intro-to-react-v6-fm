2f8b84f3a58fe0dd3ddb5fe495a53b59236f12ba
Nelo Puchades Bresó <correo@nelo.dev>
2021-05-22 13:52:35 +0200

    Added automatic redirect when catching error on Details paeg

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,8 +1,8 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
@@ -10,13 +10,17 @@ class ErrorBoundary extends Component {
   componentDidCatch(err, info) {
     //log err to sentry, azure monitor, new relic, trackjs
     console.error("ErrorBoundary caught an error", err, info);
+    setTimeout(() => this.setState({ redirect: true }), 3000);
   }
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
       return (
         <h2>
           This listing has an error.
-          <Link to="/">Click here to go back to the home page</Link>
+          <Link to="/">Click here</Link> to go back to the home page or wait
+          five seconds.
         </h2>
       );
     }
